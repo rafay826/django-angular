7d6e91249ce45c1392e3d295e6bf6ffd517dfe53
Rafay Choudhury <rafay826@gmail.com>
2017-10-21 00:11:59 -0700

    17: componetized comments

--- client/src/app/posts/posts.module.ts
+++ client/src/app/posts/posts.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 import { MaterialModule } from '@app/shared/material/material.module';
+import { CommentsComponent } from '@app/posts/comments/comments.component';
 
 const declarations = [
   PostListComponent,
   PostDetailComponent,
+  CommentsComponent
 ]
 
 @NgModule({
   declarations: [
-    ...declarations
+    ...declarations,
   ],
   imports: [
     MaterialModule,
